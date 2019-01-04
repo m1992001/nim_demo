0a287c9366727b6b53b410e2476ba675e19432d5
m1992001 <lmq813@yeah.net>
2019-01-04 17:20:45 +0800

    Initial commit

---
+++ demo/src/com/netease/nim/demo/common/util/LogHelper.java
@@ -0,0 +1,14 @@
+package com.netease.nim.demo.common.util;
+
+import com.netease.nim.uikit.common.util.log.LogUtil;
+
+/**
+ * Created by winnie on 2017/12/21.
+ */
+
+public class LogHelper {
+
+    public static void init() {
+
+    }
+}
